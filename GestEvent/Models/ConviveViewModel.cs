e63afbbbc205eef674a23870fa4f78e3bafbf1bb
Unknown <kevin.garnier01@gmail.com>
2018-02-28 09:35:14 +0100

    Commit 28/02

--- GestEvent/Models/ConviveViewModel.cs
+++ GestEvent/Models/ConviveViewModel.cs
@@ -16,6 +16,8 @@ namespace GestEvent.Models
 
         public List<Double> LatLongAdresseDepartUser { get; set; }
 
+        public List<Double> LatlongParkingDest { get; set; }
+
         public string AddresseUser { get; set; }
         public string ViewRubricUrl { get; set; }
 
